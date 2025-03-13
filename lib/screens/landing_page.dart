98742a48016d36b56bd0304e6ba1645f8076510e
Krishna Kothawale <kothawalekrishna@gmail.com>
2025-03-14 00:31:39 +0530

    Started the frontend

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -7,17 +7,52 @@ import '../admin/report_details.dart';
 import '../admin/view_reports.dart';
 import 'my_community.dart';
 
-class LandingPage extends StatelessWidget {
-
+class LandingPage extends StatefulWidget {
   final String token;
 
-  LandingPage({required this.token});  
+  LandingPage({required this.token});
 
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-        
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.red[100],
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.messenger),
+            icon: Icon(Icons.messenger_outline_rounded),
+            label: 'Community',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.warning),
+            icon:  Icon(Icons.warning_amber),
+            label: 'Report',
+          ),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.person),
+              icon: Icon(Icons.person_outlined),
+              label: 'My Reports'
+          ),
+        ],
+      ),
       appBar: AppBar(title: Text('Landing Page')),
       body: Center(
         child: Column(
@@ -25,54 +60,64 @@ class LandingPage extends StatelessWidget {
           children: <Widget>[
             Text(
               'Your Unique Token:',
-              
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             SelectableText(
-              token,  
+              widget.token,
               style: TextStyle(fontSize: 18),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                Clipboard.setData(ClipboardData(text: token));  // Copy to clipboard
+                Clipboard.setData(
+                    ClipboardData(text: widget.token)); // Copy to clipboard
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(content: Text('Token copied to clipboard!')),
                 );
               },
               child: Text('Copy to Clipboard'),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pushReplacement(
                     context,
-                    MaterialPageRoute(builder: (context) => ReportForm(token: token)),
+                    MaterialPageRoute(
+                        builder: (context) => ReportForm(token: widget.token)),
                   );
-            }, child: Text('Report')),
-            SizedBox(height: 20,),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => ViewReports()),
-                  );
-                }, child: Text('myreports')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => SpeakerDetailsScreen()),
-                  );
-                }, child: Text('community')),
+                },
+                child: Text('Report')),
+            SizedBox(
+              height: 20,
+            ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => ViewReports()),
                   );
-                }, child: Text('View')),
+                },
+                child: Text('myreports')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SpeakerDetailsScreen()),
+                  );
+                },
+                child: Text('community')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => ViewReports()),
+                  );
+                },
+                child: Text('View')),
           ],
         ),
       ),
---
+++ lib/test/test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        // onDestinationSelected: (int index) {
+        //   setState(() {
+        //     currentPageIndex = index;
+        //   });
+        // },
+        indicatorColor: Colors.amber,
+        // selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
+            label: 'Messages',
+          ),
+        ],
+      ),
+
+    );
+  }
+}
